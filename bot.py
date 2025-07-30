67a7ff105209434319bcc5a569646cbf2dc9974d
ATHARV THAKUR <62700054+atharvthakurzone@users.noreply.github.com>
2025-07-31 03:07:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -708,7 +708,7 @@ app.add_handler(conv_handler)
 
     # 4. Messages
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_menu))
-app.add_handler(MessageHandler(filters.TEXT & filters.ALL, handle_broadcast))
+#app.add_handler(MessageHandler(filters.TEXT & filters.ALL, handle_broadcast))
 app.add_handler(MessageHandler(filters.PHOTO, handle_screenshot))
 
 # Start bot with webhook
