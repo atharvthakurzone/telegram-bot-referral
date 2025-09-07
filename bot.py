c962ecbe7ec079164b33598c0bd9cc65bca655ee
ATHARV THAKUR <62700054+atharvthakurzone@users.noreply.github.com>
2025-09-07 21:32:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -321,7 +321,7 @@ if __name__ == "__main__":
 
     # Add test command handler
     app.add_handler(CommandHandler("testchat", test_support))
-
+    app.run_polling()
 
 #due weekly bonus       
 def is_weekly_bonus_due(telegram_id):
